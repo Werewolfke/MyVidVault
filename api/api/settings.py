1c78acab21bdfe7c0a9b01874b57a305848ee172
Riley Pollard <rjpollard12@gmail.com>
2025-06-30 09:21:09 -0500

    Current

--- api/api/settings.py
+++ api/api/settings.py
@@ -77,7 +77,7 @@ DATABASES = {
         'PASSWORD': os.getenv('DB_PASSWORD'),
         'HOST': os.getenv('DB_HOST', 'localhost'),
         'PORT': os.getenv('DB_PORT', '5432'),
-        'CONN_MAX_AGE': 60,  # Close connections after 60 seconds
+        'CONN_MAX_AGE': 0,  # Close connections after 60 seconds
       
     }
 }
